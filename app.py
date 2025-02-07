7eabf38d2ad031f5c130fd82df4939f6610fea4c
Sébastien Fiquet <batobatopi@gmail.com>
2025-02-07 10:47:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ selection = option_menu(
             menu_title=None,
             options = ["Accueil", "Photos"]
         )    
-    add_radio = st.radio(
+add_radio = st.radio(
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
